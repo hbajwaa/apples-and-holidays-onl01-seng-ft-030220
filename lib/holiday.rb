cb442edaead7680a7e6dbeb78eb90cfac44f1d65
Hassan Bajwa <bajwahassan3@outlook.com>
2020-03-08 10:27:54 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -84,7 +84,7 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  includes_bbq
+  includes_bbq =[]
   
   holiday_hash.each do |season, holiday|
     
